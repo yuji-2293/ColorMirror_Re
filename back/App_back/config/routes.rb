bf094ecaf28518aa737d82c4d02930e1efb1f753
inoueyuji <sikogaku2593@gmail.com>
2025-09-23 00:21:15 +0900

    learn

--- back/App_back/config/routes.rb
+++ back/App_back/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   namespace :api do
     namespace :v4, format: :json  do
-      resources :todos, only: [:index, :create, :update, :destroy]
+      resources :todos, only: [ :index, :create, :update, :destroy ]
     end
   end
   # Defines the root path route ("/")
