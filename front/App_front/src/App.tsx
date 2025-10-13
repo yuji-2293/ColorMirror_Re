3e57b5f91f4a543a7e67937e1821fee4489daa97
inoueyuji <sikogaku2593@gmail.com>
2025-10-13 13:41:11 +0900

    add/shadcn-ui

--- front/App_front/src/App.tsx
+++ front/App_front/src/App.tsx
@@ -3,8 +3,9 @@ import './App.css';
 function App() {
   return (
     <>
-      <div>
-        <p className="text-blue-500 font-bold text-2xl text-center">test中</p>
+      <div className="h-30 w-30 bg-secondary">
+        <p className="text-gray-300 font-bold text-2xl text-center">test中</p>
+        <button>Click come!!</button>
       </div>
     </>
   );
