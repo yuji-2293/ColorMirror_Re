730bad2f51a6f2246b9168e207cf1c3718797438
inoueyuji <sikogaku2593@gmail.com>
2025-10-19 20:34:16 +0900

    test/commit

--- front/App_front/src/App.tsx
+++ front/App_front/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="h-30 w-30 bg-secondary">
+      <div className="h-30 w-30 bg-sky-500">
         <p className="text-gray-300 font-bold text-2xl text-center">test中</p>
         <button>Click come!!</button>
       </div>
