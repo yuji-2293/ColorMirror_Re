04a02ca6081a3894600f4f35d0b021651929d623
inoueyuji <sikogaku2593@gmail.com>
2025-10-28 22:47:04 +0900

    test

--- front/App_front/src/App.tsx
+++ front/App_front/src/App.tsx
@@ -5,7 +5,6 @@ function App() {
     <>
       <div className="h-30 w-30 bg-sky-500">
         <p className="text-gray-300 font-bold text-2xl text-center">test中</p>
-        <button>Click come!!</button>
       </div>
     </>
   );
