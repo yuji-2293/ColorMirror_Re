9a837296d07f2f0bc1d69f20a8c3bb7ce02e132c
inoueyuji <sikogaku2593@gmail.com>
2025-11-02 18:16:24 +0900

    add/API接続テスト

--- front/App_front/src/api/colorsGetData.ts
+++ front/App_front/src/api/colorsGetData.ts
@@ -1,5 +1,7 @@
+import { API_BASE_URL } from './config';
+
 export default async function colorsGetData() {
-  const response = await fetch('http://localhost:3000/api/v1/colors');
+  const response = await fetch(`${API_BASE_URL}/colors`);
   if (!response.ok) {
     throw new Error('Failed to fetch colors data');
   }
---
+++ front/App_front/src/api/colorsPostData.ts
@@ -0,0 +1,21 @@
+import { type Color } from '../types/Color';
+import { API_BASE_URL } from './config';
+
+export default async function colorsPostData(): Promise<Color> {
+  const response = await fetch(`${API_BASE_URL}/colors`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      color_name: 'blue',
+      mood: 'モヤモヤ',
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to post color data');
+  }
+
+  return await response.json();
+}
