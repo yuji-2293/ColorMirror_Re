21609e940d63ed926fbe1f758b37335ed7c812b8
inoueyuji <sikogaku2593@gmail.com>
2025-11-04 23:17:24 +0900

    CI/テスト

--- front/App_front/src/api/config.ts
+++ front/App_front/src/api/config.ts
@@ -1,5 +1,1 @@
-// api/config.ts
-export const DEV = import.meta.env.DEV; // boolean
-export const PROD = import.meta.env.PROD; // boolean
-
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
