7243ec40d723a4ae932b10146a973efc560b7787
inoueyuji <sikogaku2593@gmail.com>
2025-12-02 22:36:34 +0900

    learn

--- front/App_front/src/app/lib/apiClient.ts
+++ front/App_front/src/app/lib/apiClient.ts
@@ -8,11 +8,14 @@ const headers = {
 
 export const ApiClient = axios.create({ baseURL, headers });
 
+// 上で生成したaxiosインスタンスに .use(成功時関数, 失敗時関数)という形でインターセプターする関数（割り込んで処理を挟む）を登録する
 ApiClient.interceptors.response.use(
+  // あらかじめaxios内で定義されてる第一引数の成功時の関数
   (response) => {
     console.log(response.data);
     return response;
   },
+  // あらかじめaxios内で定義されてる第二引数の失敗時の関数
   (error) => {
     console.log(error);
     switch (error.response?.status) {
