4dba16bca4eacd08932bb0ea093241affc802e2d
inoueyuji <sikogaku2593@gmail.com>
2025-12-17 16:47:43 +0900

    custum/devise

--- back/App_back/app/controllers/application_controller.rb
+++ back/App_back/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+        # SetUserByTokenという拡張機能(CORSやCookie)を利用する
         include DeviseTokenAuth::Concerns::SetUserByToken
   def render_api(data:, meta: {}, error: nil, status: :ok)
     render json: { data:, meta:, error: }, status:
