3452db398dbf7f3e5a3add729cf6cae9410ef0ec
inoueyuji <sikogaku2593@gmail.com>
2026-01-11 16:12:39 +0900

    learn

--- front/App_front/src/App.tsx
+++ front/App_front/src/App.tsx
@@ -4,19 +4,25 @@ import { Home } from '@/pages/Home';
 import { SignIn } from '@/pages/SignIn';
 import { SignUp } from '@/pages/SignUp';
 import { SignOut } from '@/pages/SignOut';
+import PublicLayout from '@/pages/PublicLayout';
 import PrivateLayout from '@/pages/PrivateLayout';
 export default function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          {/* 公開レイアウト */}
+          <Route element={<PublicLayout />}>
+            <Route path="signIn" element={<SignIn />} />
+            <Route path="signUp" element={<SignUp />} />
+          </Route>
+
+          {/* 認証後のレイアウト */}
           <Route path="/" element={<PrivateLayout />}>
             <Route index element={<Home />} />
             <Route path="signOut" element={<SignOut />} />
             <Route path="*" element={<h1>StatusCode-404 Not Found Page</h1>} />
           </Route>
-          <Route path="signIn" element={<SignIn />} />
-          <Route path="signUp" element={<SignUp />} />
         </Routes>
       </Router>
     </div>
