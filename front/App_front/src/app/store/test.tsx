0feb5519a8809837e22b873139faaf5f3df820cb
inoueyuji <sikogaku2593@gmail.com>
2026-01-25 21:29:56 +0900

    learn

--- front/App_front/src/app/store/test.tsx
+++
@@ -1,7 +0,0 @@
-import { useStore } from '@/app/store/useStore';
-
-export function TestComponent() {
-  const increment = useStore((state) => state.increment);
-
-  return <button onClick={increment}>+1</button>;
-}
