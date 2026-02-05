a11d51927be85ff33d99adcc6a7374dca1f9ef81
inoueyuji <sikogaku2593@gmail.com>
2026-02-05 22:41:16 +0900

    learn

--- front/App_front/src/app/features/auth/components/signUpCard.tsx
+++ front/App_front/src/app/features/auth/components/signUpCard.tsx
@@ -1,51 +1,22 @@
-import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardTitle,
-  CardHeader,
-} from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardTitle, CardHeader } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { type AuthParams } from '@/app/features/auth/types/authType';
-import { signUp } from '@/app/features/auth/api/auth';
-import { useNavigate } from 'react-router-dom';
-
+import { useSignUp } from '@/app/features/auth/hooks/useSignUp';
 export default function SignUpCard() {
-  // ユーザー登録用の状態管理state
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [password_confirmation, setPasswordConfirmation] = useState('');
-  const navigate = useNavigate();
+  // カスタムフックから状態と関数を取得
+  const {
+    name,
+    setName,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    password_confirmation,
+    setPasswordConfirmation,
+    handleSignUp,
+  } = useSignUp();
 
-  // サインアップ処理(signUp api呼び出し)
-  const handleSignUp = async (e: React.FormEvent) => {
-    e.preventDefault();
-    // passwordとpassword_confirmationが一致するか確認
-    if (password !== password_confirmation) {
-      alert('パスワードとパスワード確認が一致しません。');
-      return;
-    }
-    // ユーザー登録用パラメータ
-    const params: AuthParams = {
-      name,
-      email,
-      password,
-      password_confirmation: password_confirmation,
-    };
-    try {
-      await signUp(params);
-      alert('ユーザー登録が完了しました。サインインしてください。');
-      navigate('/signin');
-    } catch (error) {
-      alert('ユーザー登録に失敗しました。');
-      console.error(error);
-    }
-  };
   return (
     <Card>
       <CardHeader>
@@ -94,11 +65,11 @@ export default function SignUpCard() {
               placeholder="パスワードを再入力"
             />
           </div>
-          <CardFooter>
+          <div>
             <Button type="submit" className="w-full">
               アカウントを作成
             </Button>
-          </CardFooter>
+          </div>
         </form>
       </CardContent>
     </Card>
---
+++ front/App_front/src/app/features/auth/hooks/useSignUp.tsx
@@ -0,0 +1,49 @@
+import { type AuthParams } from '@/app/features/auth/types/authType';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signUp } from '@/app/features/auth/api/auth';
+
+export const useSignUp = () => {
+  // ユーザー登録用の状態管理state
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password_confirmation, setPasswordConfirmation] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // passwordとpassword_confirmationが一致するか確認
+    if (password !== password_confirmation) {
+      alert('パスワードとパスワード確認が一致しません。');
+      return;
+    }
+
+    // ユーザー登録用パラメータ
+    const params: AuthParams = {
+      name,
+      email,
+      password,
+      password_confirmation: password_confirmation,
+    };
+    try {
+      await signUp(params);
+      alert('ユーザー登録が完了しました。サインインしてください。');
+      navigate('/signin');
+    } catch (error) {
+      alert('ユーザー登録に失敗しました。');
+      console.error(error);
+    }
+  };
+  return {
+    name,
+    setName,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    password_confirmation,
+    setPasswordConfirmation,
+    handleSignUp,
+  };
+};
