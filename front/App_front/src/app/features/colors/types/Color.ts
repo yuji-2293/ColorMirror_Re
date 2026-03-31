a6148744dd46ca3faf549edeb422dbae8ab8bf79
inoueyuji <sikogaku2593@gmail.com>
2026-03-31 14:50:02 +0900

    learn

--- front/App_front/src/app/features/colors/types/Color.ts
+++ front/App_front/src/app/features/colors/types/Color.ts
@@ -4,26 +4,27 @@ export interface Color {
   mood: string;
   createdAt: string;
 }
-
+// APIからのレスポンスの型定義
 export type ColorResponse = {
   data: Color[];
 };
-
+// 新しいカラーを作成するためのリクエストの型定義
 export interface CreateColorParams {
   color: {
     colorName: string;
     mood: string;
   };
 }
-
+// ムードに基づいてカラーを生成するためのリクエストの型定義
 export interface GenerateMoodParams {
   mood: string;
 }
-
+// ムードに基づいて生成されたカラーの型定義
 export type generateColors = {
   hex: string;
   name: string;
 };
+// ムードに基づいて生成されたカラーのレスポンスの型定義
 export type GenerateResponse = {
   data: {
     generatedColor: generateColors[];
--- front/App_front/src/pages/private/components/Home.tsx
+++ front/App_front/src/pages/private/components/Home.tsx
@@ -1,4 +1,3 @@
-import { ColorsCreate } from '@/app/features/colors/components/colorsCreate';
 import { ColorsIndex } from '@/app/features/colors/components/colorsIndex';
 import { ColorsForm } from '@/app/features/colors/components/colorsForm';
 export const Home = () => {
@@ -7,7 +6,6 @@ export const Home = () => {
       <h1>ホーム</h1>
       <ColorsForm />
       <ColorsIndex />
-      <ColorsCreate />
     </div>
   );
 };
