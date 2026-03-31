15b781a975ab13ad9c999cbcdea5af7202d56c2f
inoueyuji <sikogaku2593@gmail.com>
2026-03-31 23:41:28 +0900

    learn

--- front/App_front/src/app/features/responses/components/responsesForm.tsx
+++ front/App_front/src/app/features/responses/components/responsesForm.tsx
@@ -1,6 +1,6 @@
 import { type ColorsFormProps } from '@/app/features/colors/types/Color';
-import { useGenerateResponse } from '@/app/features/Responses/hooks/useGenerateResponse';
-import { type GenerateResponseDataParams } from '@/app/features/Responses/types/Response';
+import { useGenerateResponse } from '@/app/features/responses/hooks/useGenerateResponse';
+import { type GenerateResponseDataParams } from '@/app/features/responses/types/response';
 export const ResponsesForm = ({ mood, selectedColor }: ColorsFormProps) => {
   const { aiResponseData, generateResponse, isPending } = useGenerateResponse();
   const handleGenerateResponse = () => {
@@ -17,7 +17,21 @@ export const ResponsesForm = ({ mood, selectedColor }: ColorsFormProps) => {
       <h1>ResponsesForm</h1>
       <p>{mood}</p>
       <p>{selectedColor}</p>
-      <button onClick={handleGenerateResponse}>AI生成開始</button>
+      <button onClick={handleGenerateResponse}>
+        AI生成開始 disabled={(!mood && !selectedColor) || isPending}
+      </button>
+
+      {isPending && (
+        <div className="flex justify-center items-center w-24 h-24 bg-white rounded-2xl shadow-2xl">
+          <div
+            className="flex justify-center items-center animate-pulse w-20 h-20 border-4 rounded-full"
+            role="status"
+          >
+            <p className="text-center text-sm">生成中...</p>
+          </div>
+        </div>
+      )}
+
       <p>{aiResponseData}</p>
     </div>
   );
