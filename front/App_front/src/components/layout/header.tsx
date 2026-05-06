d104a307241d4aa3a0f772484f25a24fdfcd69f3
inoueyuji <sikogaku2593@gmail.com>
2026-05-06 23:37:38 +0900

    learn

--- front/App_front/src/components/layout/header.tsx
+++ front/App_front/src/components/layout/header.tsx
@@ -11,7 +11,6 @@ export const Header = () => {
   const isAuthenticated = authStatus === 'authenticated';
   const currentUserName = isAuthenticated ? user?.name : 'Guest';
   // ログアウト処理
-
   const handleLogout = async () => {
     try {
       await signOut(); // サーバー側のログアウトAPIを呼び出す
@@ -22,23 +21,28 @@ export const Header = () => {
 
   return (
     <header className="bg-gradient w-full px-6 py-4 mx-auto">
-      <div className="flex justify-between items-center">
-        <h1 className="text-3xl font-bold text-white">Color Mirror_Re</h1>
-        {isAuthenticated && (
-          <div className="mt-2">
-            <button
-              onClick={handleLogout}
-              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-            >
-              ログアウト
-            </button>
+      <div className="flex flex-col items-center justify-center sm:flex-row sm:justify-between ">
+        <div className="header-text">
+          <p className="text-3xl font-bold  text-gray-500">Color Mirror_Re</p>
+        </div>
+
+        <div className="header-content sm:flex sm:flex-col sm:items-center gap-2">
+          {isAuthenticated && (
+            <div className="">
+              <button
+                onClick={handleLogout}
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              >
+                ログアウト
+              </button>
+            </div>
+          )}
+          <div className="text-red-500 text-md underline font-bold text-right">
+            {isAuthenticated
+              ? `ログイン中のユーザー:  ${currentUserName} さん`
+              : 'ゲストさん ログインをお願いします.'}
           </div>
-        )}
-      </div>
-      <div className="text-red-500 text-md mt-1 font-bold text-right">
-        {isAuthenticated
-          ? `ログイン中:  ${currentUserName} さん`
-          : '未ログイン: ゲストさん ログインをお願いします.'}
+        </div>
       </div>
     </header>
   );
