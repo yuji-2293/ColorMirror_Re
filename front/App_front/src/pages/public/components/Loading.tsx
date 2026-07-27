de3ad2a9470f7936c3939e160d6be45d9e12f991
inoueyuji <sikogaku2593@gmail.com>
2026-07-27 19:32:13 +0900

    learn

--- front/App_front/src/pages/public/components/Loading.tsx
+++ front/App_front/src/pages/public/components/Loading.tsx
@@ -2,7 +2,7 @@ export const Loading = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gradient gap-4">
       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
-      <p>初期起動に時間がかかることがあります</p>
+      <p>loading中です...</p>
     </div>
   );
 };
