7ad3d7a040ac67658566bea0eddf6b25b19a9877
inoueyuji <sikogaku2593@gmail.com>
2026-08-07 23:32:28 +0900

    test

--- front/App_front/vite.config.ts
+++ front/App_front/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
@@ -6,6 +6,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setUp.ts',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
