587056a9d220622f64881064311490e1aa27d512
inoueyuji <sikogaku2593@gmail.com>
2026-08-17 12:20:15 +0900

    runTestCase

--- front/App_front/src/app/features/responses/components/createForm.test.tsx
+++ front/App_front/src/app/features/responses/components/createForm.test.tsx
@@ -1,8 +1,12 @@
 import { describe, it, expect, vi } from 'vitest';
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { CreateForm } from '@/app/features/responses/components/createForm';
 import { type CreateFormProps } from '@/app/features/responses/types/Response';
+import { toast } from 'sonner';
+// mockUseCreateResponseの振る舞いを定義する、本来のhooks自体が返すオブジェクトを返すために定義する
 const { mockCreateResponse, mockUseCreateResponse } = vi.hoisted(() => ({
+  // createResponseのモック関数を定義する
   mockCreateResponse: vi.fn(),
   mockUseCreateResponse: vi.fn(() => ({
     createResponse: mockCreateResponse,
@@ -11,9 +15,17 @@ const { mockCreateResponse, mockUseCreateResponse } = vi.hoisted(() => ({
   })),
 }));
 
+// hooks自体をモック化する。
 vi.mock('@/app/features/responses/hooks/useCreateResponse', () => ({
   useCreateResponse: mockUseCreateResponse,
 }));
+
+vi.mock('sonner', () => ({
+  toast: {
+    success: vi.fn(),
+  },
+}));
+
 const defaultProps: CreateFormProps = {
   mood: 'ワクワク',
   setMood: vi.fn(),
@@ -28,6 +40,16 @@ const defaultProps: CreateFormProps = {
   generateResponse: vi.fn(),
   isSuccess: false,
   isPending: false,
+};
+
+const mockResponseParams = {
+  color: {
+    mood: 'ワクワク',
+    color_name: '#FF5733',
+  },
+  response: {
+    ai_response: 'AIコメント',
+  },
 };
 
 describe('CreateForm', () => {
@@ -51,11 +73,69 @@ describe('CreateForm', () => {
         {...defaultProps}
         mood={defaultProps.mood}
         selectedColorName={defaultProps.selectedColorName}
-        aiResponseData={defaultProps.aiResponseData}
       />
     );
-
     const button = screen.getByRole('button', { name: '保存する' });
     expect(button).toBeDisabled();
+  });
+  it('isPendingがtrueの時、ボタンを押しても処理が走らない', async () => {
+    mockUseCreateResponse.mockReturnValue({
+      createResponse: mockCreateResponse,
+      isSuccess: false,
+      isPending: true,
+    });
+    render(
+      <CreateForm
+        {...defaultProps}
+        mood={defaultProps.mood}
+        selectedColorName={defaultProps.selectedColorName}
+      />
+    );
+    const button = screen.getByRole('button', { name: '保存する' });
+    const user = userEvent.setup();
+    await user.click(button);
+    expect(mockCreateResponse).not.toHaveBeenCalled();
+  });
+  it('isPendingがtrueの時、スピナーが表示される', () => {
+    mockUseCreateResponse.mockReturnValue({
+      createResponse: mockCreateResponse,
+      isSuccess: false,
+      isPending: true,
+    });
+    render(<CreateForm {...defaultProps} />);
+    expect(screen.getByTestId('spinner')).toBeInTheDocument();
+  });
+
+  it('保存ボタンをクリックすると、paramsを作成して、createResponseが呼ばれる', async () => {
+    mockUseCreateResponse.mockReturnValue({
+      createResponse: mockCreateResponse,
+      isSuccess: false,
+      isPending: false,
+    });
+    render(<CreateForm {...defaultProps} />);
+    const user = userEvent.setup();
+    const button = screen.getByRole('button', { name: '保存する' });
+    await user.click(button);
+    expect(mockCreateResponse).toHaveBeenCalledWith(mockResponseParams);
+  });
+  it('isSuccessがtrueの時、トースト通知が表示される', () => {
+    mockUseCreateResponse.mockReturnValue({
+      createResponse: mockCreateResponse,
+      isSuccess: true,
+      isPending: false,
+    });
+    render(<CreateForm {...defaultProps} />);
+    expect(toast.success).toHaveBeenCalledWith('保存が成功しました！');
+  });
+  it('isSuccessがtrueの時、フォームの状態がリセットされる', () => {
+    mockUseCreateResponse.mockReturnValue({
+      createResponse: mockCreateResponse,
+      isSuccess: true,
+      isPending: false,
+    });
+    render(<CreateForm {...defaultProps} />);
+    expect(defaultProps.resetAll).toHaveBeenCalled();
+    expect(defaultProps.resetAiResponseData).toHaveBeenCalled();
+    expect(defaultProps.resetColors).toHaveBeenCalled();
   });
 });
