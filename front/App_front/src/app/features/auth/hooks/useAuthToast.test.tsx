9f8f3d0d8ff65c2d175215c779d6f42c8c321a76
inoueyuji <sikogaku2593@gmail.com>
2026-08-28 00:15:03 +0900

    runTestCase

--- front/App_front/src/app/features/auth/hooks/useAuthToast.test.tsx
+++ front/App_front/src/app/features/auth/hooks/useAuthToast.test.tsx
@@ -11,22 +11,24 @@ vi.mock('sonner', () => ({
   },
 }));
 
-vi.mock('react-router-dom', () => ({
-  useLocation: vi.fn(() => ({
-    state: {
-      email: 'test@example.com',
-    } as SignInNavState,
-  })),
-  useNavigate: vi.fn(() => vi.fn()),
-}));
-
 const mockClearRedirectedReason = vi.fn();
 // zustandのuseAuthStoreをモック化して、clearRedirectedReason関数を返すようにする
 vi.mock('@/app/store/useAuthStore', () => ({
   useAuthStore: vi.fn((state) =>
     state({
-    clearRedirectedReason: mockClearRedirectedReason,
-  })),
+      clearRedirectedReason: mockClearRedirectedReason,
+    })
+  ),
+}));
+
+const { mockUseLocation, mockUseNavigate } = vi.hoisted(() => ({
+  mockUseLocation: vi.fn(),
+  mockUseNavigate: vi.fn(),
+}));
+
+vi.mock('react-router-dom', () => ({
+  useLocation: mockUseLocation,
+  useNavigate: () => mockUseNavigate,
 }));
 
 describe('useAuthToast', () => {
@@ -35,9 +37,51 @@ describe('useAuthToast', () => {
   });
 
   it('stateにemailがある場合、登録完了のトーストが表示されること', async () => {
+    mockUseLocation.mockReturnValue({
+      state: {
+        email: 'e',
+      } as SignInNavState,
+    });
+
     renderHook(() => useAuthToast());
     await waitFor(() => {
       expect(toast.success).toHaveBeenCalledWith('登録が完了しました。ログインしてください。');
+    });
+  });
+
+  it('stateにtoast=login_requireの時、ログインが必要なトーストが表示されること', async () => {
+    mockUseLocation.mockReturnValue({
+      state: {
+        toast: 'login_require',
+      } as SignInNavState,
+    });
+    renderHook(() => useAuthToast());
+    await waitFor(() => {
+      expect(toast.error).toHaveBeenCalledWith('ログインが必要です。');
+    });
+  });
+
+  it('stateにtoast=logged_outの時、ログアウト完了のトーストが表示されること', async () => {
+    mockUseLocation.mockReturnValue({
+      state: {
+        toast: 'logged_out',
+      } as SignInNavState,
+    });
+    renderHook(() => useAuthToast());
+    await waitFor(() => {
+      expect(toast.success).toHaveBeenCalledWith('ログアウトしました。');
+    });
+  });
+
+  it('toastを表示した後、clearRedirectedReasonが呼ばれること', async () => {
+    mockUseLocation.mockReturnValue({
+      state: {
+        toast: 'logged_out',
+      } as SignInNavState,
+    });
+    renderHook(() => useAuthToast());
+    await waitFor(() => {
+      expect(mockClearRedirectedReason).toHaveBeenCalled();
     });
   });
 });
