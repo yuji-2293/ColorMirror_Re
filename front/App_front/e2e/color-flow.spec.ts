67c011acb41dac45d54a84e03ebabb21650b3e5c
inoueyuji <sikogaku2593@gmail.com>
2026-09-06 14:03:05 +0900

    runTestCase

--- front/App_front/e2e/color-flow.spec.ts
+++ front/App_front/e2e/color-flow.spec.ts
@@ -13,4 +13,19 @@ test('主要機能のフローが正常に動作する', async ({ page }) => {
     name: /を選択/,
   });
   await colorButtonSelect.first().click();
+  await page.getByRole('button', { name: 'AI生成開始', exact: true }).click();
+  await expect(page.getByRole('button', { name: 'AIコメント再生成', exact: true })).toBeVisible();
+
+  // 保存したデータが一覧に表示されるかテスト
+  const responsePromise = page.waitForResponse(
+    (response) => response.url().includes('/responses') && response.request().method() === 'POST'
+  );
+  await page.getByRole('button', { name: '保存する', exact: true }).click();
+  await expect(page.getByRole('button', { name: '保存する', exact: true })).toBeDisabled();
+  const response = await responsePromise;
+  const request = response.request();
+  const params = request.postDataJSON();
+  await expect(page.getByText(params.color.mood, { exact: true })).toBeVisible();
+  await expect(page.getByText(params.response.ai_response)).toBeVisible();
+  console.log(params);
 });
